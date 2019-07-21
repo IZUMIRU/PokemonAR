9c3ec5c415fbdc038738154457692bdad13d91d7
IZUMIRU <izumiruryousuke@gmail.com>
2019-07-21 17:47:43 +0900

    first commit

--- PokemonAR/ViewController.swift
+++ PokemonAR/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  PokemonAR
 //
-//  Created by PC-267 on 2019/07/16.
+//  Created by IZUMIRU on 2019/07/16.
 //  Copyright © 2019 IZUMIRU. All rights reserved.
 //
 
@@ -12,64 +12,62 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    // ARSCNViewの設定
     @IBOutlet var sceneView: ARSCNView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
+        // fps情報などを表示
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        // デバッグ用に特徴点を表示
+        sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        //Viewに初期化したsceneをセット
+        sceneView.scene = SCNScene()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
 
-        // Run the view's session
+        // Sessionの設定
+        let configuration = ARWorldTrackingConfiguration()
+        // 検知対象を指定する。水平面。（.verticalは垂直面）
+        configuration.planeDetection = .horizontal
+        // 空間から光の情報を取得し画面上のライトの情報に適応
+        configuration.isLightEstimationEnabled = true
+        // sessionをスタート
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
+
+        // sessionをストップ
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    // ARSCNViewDelegateのメソッド
+    // 平面を新たに検知した際に、呼ばれるrenderer(_:didAdd:for:)で、検知したNodeに3DオブジェクトのNodeを追加
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {fatalError()}
+        //sceneとnodeを読み込み
+        guard let scene = SCNScene(named: "model.scn", inDirectory: "art.scnassets/Raikou") else {fatalError()}
+        guard let bearNode = scene.rootNode.childNode(withName: "SketchUp", recursively: true) else {fatalError()}
+
+        // nodeのスケールを調整する
+        let (min, max) = bearNode.boundingBox
+        let w = CGFloat(max.x - min.x)
+        // 0.1mを基準にした縮尺を計算
+        let magnification = 0.1 / w
+        bearNode.scale = SCNVector3(magnification, magnification, magnification)
+        // nodeのポジションを設定
+        bearNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+
+        // 作成したノードを追加
+        DispatchQueue.main.async(execute: {
+            node.addChildNode(bearNode)
+        })
     }
 }
